c0253cf0f185d1ac76a990f610e2636cd5a4f1f4
Royston <roystonad2004@gmail.com>
2024-10-18 23:04:05 +0530

    login update

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,11 +1,34 @@
-
+import React ,{useState} from "react";
 
 const Login = () => {
+
+    const [inputData,setInputData] = useState('');
+    const [inputPassword,setInputPassword] =useState('');
+
+    const handleInputChange = (e) =>{
+        setInputData(e.target.value);
+        setInputPassword(e.target.value);
+    } 
+    const handleSubmit=async (e)=>{
+        e.preventDefault();
+    
+
+    await fetch('http://localhost:3000/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: inputData },{password:inputPassword}),  // Send data to the server
+      });
+    };
+
+
   return (
     <div>
-      <form method="POST" action="">
-        <input type="text" name="username" required></input>
-        <input type="password" name="password" required></input>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputData} onChange={handleInputChange} placeholder="Enter username" />
+        <input type="password" value={inputData} onChange={handleInputChange} placeholder="Enter username" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
