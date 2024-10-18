0107f66770a695735606ab5f4549449ddeff0000
Royston <roystonad2004@gmail.com>
2024-10-18 22:39:50 +0530

    all done

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -3,7 +3,10 @@
 const Login = () => {
   return (
     <div>
-      
+      <form method="POST" action="">
+        <input type="text" name="username" required></input>
+        <input type="password" name="password" required></input>
+      </form>
     </div>
   )
 }
