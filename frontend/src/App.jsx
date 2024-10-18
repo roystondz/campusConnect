ad18c586f5c5b316feda6a47baae3717e70c9750
Royston <roystonad2004@gmail.com>
2024-10-18 22:51:25 +0530

    LOGIN ADDED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
+import Login from "../components/Login";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Login/>
   )
 }
