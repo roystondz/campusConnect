b0c8dc4166b658bcad841f3b3828df240e2219a4
Royston <roystonad2004@gmail.com>
2024-10-19 11:01:28 +0530

    Modified

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -6,8 +6,11 @@ const Login = () => {
 
     const handleInputChange = (e) =>{
         setInputData(e.target.value);
-        setInputPassword(e.target.value);
-    } 
+        
+    }
+    const handlePasswordChange=(e)=>{
+      setInputPassword(e.target.value);
+    }
     const handleSubmit=async (e)=>{
         e.preventDefault();
     
@@ -23,9 +26,9 @@ const Login = () => {
 
 
   return (
-    <div className="flex justify-center items-center h-screen bg-purple-100 dark:bg-gray-800">
-      <div className="bg-white dark:bg-gray-700 p-8 rounded-lg shadow-md w-96">
-        <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb text-center mb-6">Welcome back</h2>
+    <div className="flex items-center justify-center h-screen bg-purple-100 dark:bg-gray-800">
+      <div className="p-8 bg-white rounded-lg shadow-md dark:bg-gray-700 w-96">
+        <h2 className="mb-6 text-2xl font-bold text-center text-gray-900 dark:text-white mb">Welcome back</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <input 
             type="text" 
@@ -37,14 +40,14 @@ const Login = () => {
           <input 
             type="password" 
             value={inputPassword} 
-            onChange={handleInputChange} 
+            onChange={handlePasswordChange} 
             placeholder="Enter password" 
             className="w-full px-3 py-2 border rounded-md dark:bg-gray-600 dark:text-white dark:border-gray-500"
           />
           <h3 className="dark:text-zinc-300"><a href=""></a>Forgot Password?</h3>
           <button 
             type="submit" 
-            className="w-full bg-purple-500 text-white py-2 rounded-md hover:bg-purple-600 transition-colors duration-300"
+            className="w-full py-2 text-white transition-colors duration-300 bg-purple-500 rounded-md hover:bg-purple-600"
           >
             
             Submit </button>
