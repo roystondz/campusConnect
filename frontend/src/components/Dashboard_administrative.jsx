14dd0920c26c6a53d96123895f359e5647c9b505
PACHITRA <95424607+PACHITRA@users.noreply.github.com>
2024-10-22 23:10:46 +0530

    Update Dashboard_administrative.jsx

--- frontend/src/components/Dashboard_administrative.jsx
+++ frontend/src/components/Dashboard_administrative.jsx
@@ -1,4 +1,4 @@
-import Sidebar from '../components/sidebar'; // Assuming you have a Sidebar component
+import Sidebar from '../components/Sidebar'; // Assuming you have a Sidebar component
 
 const Dashboard = () => {
   return (
